ee81073aef88c5aaf2dd6b14cb778a6a03c35285
selique <judeudemolidor@hotmail.com>
2015-06-01 23:05:37 +0000

    atualizando versao antiga (projeto ja em andamento) *desenvolver placar, pontuacao ja em contagem*

---
+++ tukano/Assets/Scripts/RotateCube.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateCube : MonoBehaviour {
+
+
+	public float vel_x;
+	public float vel_y;
+	public float vel_z;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3 (vel_x, vel_y, vel_z) * Time.deltaTime); 
+
+	}
+
+
+}
